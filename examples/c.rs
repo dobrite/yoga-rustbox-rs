f3115b1e263da4371b1f9bcf0eaed4ded81a9820
David Obrite <david@ministrycentered.com>
2017-02-11 08:28:20 -0800

    chore(examples): fix 'em

--- examples/c.rs
+++ examples/c.rs
@@ -4,6 +4,7 @@ extern crate yoga_rustbox;
 extern crate yoga_wrapper;
 
 use yoga::Backend;
+use yoga::Renderable;
 
 use std::error::Error;
 use std::default::Default;
@@ -17,20 +18,20 @@ fn main() {
         Result::Err(e) => panic!("{}", e),
     };
 
-    let mut root = yoga_wrapper::Node::new();
+    let mut text = yoga::Text::new("Yo!");
+    text.set_height(3.0);
+    text.set_align_self(yoga_wrapper::Align::Center);
+    text.set_flex_grow(1.0);
+
+    let mut image = yoga::View::new();
+    image.set_width(8.0);
+    image.set_margin(yoga_wrapper::Edge::End, 2.0);
+
+    let mut root = yoga::View::new();
     root.set_width(50.0);
     root.set_height(12.0);
     root.set_flex_direction(yoga_wrapper::FlexDirection::Row);
     root.set_padding(yoga_wrapper::Edge::All, 2.0);
-
-    let mut image = yoga_wrapper::Node::new();
-    image.set_width(8.0);
-    image.set_margin(yoga_wrapper::Edge::End, 2.0);
-
-    let mut text = yoga_wrapper::Node::new();
-    text.set_height(3.0);
-    text.set_align_self(yoga_wrapper::Align::Center);
-    text.set_flex_grow(1.0);
 
     root.insert_child(&image, 0);
     root.insert_child(&text, 1);
--- examples/text.rs
+++ examples/text.rs
@@ -4,6 +4,7 @@ extern crate yoga_rustbox;
 extern crate yoga_wrapper;
 
 use yoga::Backend;
+use yoga::Renderable;
 
 use std::error::Error;
 use std::default::Default;
@@ -17,17 +18,13 @@ fn main() {
         Result::Err(e) => panic!("{}", e),
     };
 
-    let be = yoga_rustbox::Backend::new(&rustbox);
+    let text = yoga::Text::new("Yo!!!");
 
-    let mut root = yoga_wrapper::Node::new();
+    let mut root = yoga::View::new();
     root.set_width(50.0);
     root.set_height(12.0);
     root.set_flex_direction(yoga_wrapper::FlexDirection::Row);
     root.set_padding(yoga_wrapper::Edge::All, 2.0);
-
-    let mut text = yoga_wrapper::Node::new();
-    text.set_measure_func(yoga_wrapper::measure);
-    text.set_context(&mut be.create_context("Yo!!!"));
 
     root.insert_child(&text, 0);
 
