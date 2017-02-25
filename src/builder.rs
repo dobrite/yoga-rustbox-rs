b4e8ced0aaae04f1f15b9be854dc174c684a8bfc
David Obrite <david@ministrycentered.com>
2017-02-25 07:26:44 -0800

    fix(Builder): return box; fix lifetimes

--- src/builder.rs
+++ src/builder.rs
@@ -15,15 +15,17 @@ impl Builder {
 }
 
 impl<'meas> yoga::Builds<'meas, rustbox::Color> for Builder {
-    fn create_context<'text>(&'meas self, text: &'text str) -> yoga_wrapper::Context<'text, 'meas> {
-        yoga_wrapper::Context::new(text, &self.measurer)
+    fn create_context<'text>(&'meas self,
+                             text: &'text str)
+                             -> Box<yoga_wrapper::Context<'text, 'meas>> {
+        Box::new(yoga_wrapper::Context::new(text, &self.measurer))
     }
 
     fn view<'r>(&self) -> yoga::View<'r, rustbox::Color> {
         yoga::View::new()
     }
 
-    fn text<'t, 'a: 't>(&'a self, text: &'t str) -> yoga::Text<'t, rustbox::Color> {
-        yoga::Text::new(text, &mut self.create_context(text))
+    fn text<'text>(&'meas self, text: &'text str) -> yoga::Text<'text, 'meas, rustbox::Color> {
+        yoga::Text::new(text, self.create_context(text))
     }
 }
