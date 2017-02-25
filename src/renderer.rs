3fdba786a656b18cfaf7498ea368a21442396d30
David Obrite <david@ministrycentered.com>
2017-02-25 11:53:16 -0800

    feat(renderer): add color; update to render text

--- src/renderer.rs
+++ src/renderer.rs
@@ -22,21 +22,30 @@ impl<'rbox> Renderer<'rbox> {
         let top = node.get_layout_top() as usize;
         let left = node.get_layout_left() as usize;
 
-        let color = node.get_color();
+        let color = match *node.get_color() {
+            Some(c) => c,
+            None => rustbox::Color::White,
+        };
+
         let background_color = match *node.get_background_color() {
             Some(yoga::style::BackgroundColor::Transparent) => rustbox::Color::Black,
             Some(yoga::style::BackgroundColor::Color(c)) => c,
             None => rustbox::Color::Black,
         };
 
-        for y in top..(top + height) {
-            self.rustbox.print(left,
-                               y,
-                               rustbox::RB_BOLD,
-                               rustbox::Color::White, // TODO use color
-                               background_color,
-                               &format!("{:1$}", "", width));
+        if let Some(text) = node.get_text() {
+            self.rustbox.print(left, top, rustbox::RB_BOLD, color, background_color, text);
+        } else {
+            for y in top..(top + height) {
+                self.rustbox.print(left,
+                                   y,
+                                   rustbox::RB_BOLD,
+                                   color,
+                                   background_color,
+                                   &format!("{:1$}", "", width));
+            }
         }
+
 
         for i in 0..node.get_child_count() {
             let child = node.get_child(i).unwrap();
